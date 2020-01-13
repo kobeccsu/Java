328df291d86b4968541e863bd06fba159d918d2f
zhoulei <zhoulei@10.21.16.123>
2020-01-13 23:19:34 +0800

    change to read rds

--- Web1/src/com/leizhou/dal/DB.java
+++ Web1/src/com/leizhou/dal/DB.java
@@ -8,15 +8,15 @@ import java.sql.SQLException;
 
 public class DB {
 	// mysql
-	final String connecString="jdbc:mysql://127.0.0.1:3306/mall?useUnicode=true&characterEncoding=utf-8&useSSL=false&allowPublicKeyRetrieval=true";
-	final String username = "root";
-	final String password = "zl7758258";
+	//final String _connecString="jdbc:mysql://127.0.0.1:3306/mall?useUnicode=true&characterEncoding=utf-8&useSSL=false&allowPublicKeyRetrieval=true";
+	final String _username = "root";
+	final String _password = "zl7758258";
 	
 	public boolean insert(String sql) {
 		boolean result = false;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection(connecString, username, password);
+			Connection con = this.getConnection();
 			Statement stmt = con.createStatement();
 			result = stmt.execute(sql);
 			con.close();
@@ -30,7 +30,7 @@ public class DB {
 		ResultSet result = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection(connecString, username, password);
+			Connection con = this.getConnection();
 			Statement stmt = con.createStatement();
 			result = stmt.executeQuery(sql);
 			con.close();
@@ -41,6 +41,19 @@ public class DB {
 	}
 	
 	public Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(connecString, username, password);
+		String dbName = System.getenv("RDS_DB_NAME"); 
+		dbName = dbName != null ? dbName : "mall";
+	    String userName = System.getenv("RDS_USERNAME");
+	    userName = userName != null ? userName : "root";
+	    String password = System.getenv("RDS_PASSWORD");
+	    password = password != null ? password : _password;
+	    String hostname = System.getenv("RDS_HOSTNAME");
+	    hostname = hostname != null ? hostname : "127.0.0.1";
+	    String port = System.getenv("RDS_PORT");
+	    port = port != null ? port : "3306";
+	    String jdbcUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName 
+	    		+ "?user=" + userName + "&password=" + password;
+		
+	    return DriverManager.getConnection(jdbcUrl);
 	}
 }
