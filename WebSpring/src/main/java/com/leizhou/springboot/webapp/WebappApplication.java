c3c373a9503c2f87ba8644f7cc5b8377a0a5eed9
LeiZhou <Lei_Zhou@epam.com>
2020-04-21 17:31:47 +0800

    use framework

---
+++ WebSpring/src/main/java/com/leizhou/springboot/webapp/WebappApplication.java
@@ -0,0 +1,21 @@
+package com.leizhou.springboot.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String hello(@RequestParam(value="name", defaultValue = "World") String name) {
+		return String.format("Hello %s", name);
+	}
+}
