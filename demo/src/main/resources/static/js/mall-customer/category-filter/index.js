f5b93f99f05843d855c05fac7b13d19f0265b1f8
lei_zhou <lei_zhou@epcnszxw0352.princeton.epam.com>
2020-07-07 17:10:46 +0800

    index page

--- demo/src/main/resources/static/js/mall-customer/category-filter/index.js
+++ demo/src/main/resources/static/js/mall-customer/category-filter/index.js
@@ -3,26 +3,45 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 import '../../../css/mall-customer/category-filter.css'
+import { getParameterByName } from '../../common/util'
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            attrList:[]
+        }
+
+    }
+
+    loadAttrs() {
+        axios.get("/attr/list?categoryId=" + getParameterByName("categoryId"))
+            .then((response) => {
+                this.setState({ attrList: [] });
+                this.setState({ attrList: response.data });
+            })
+    }
+
+    componentDidMount(){
+        this.loadAttrs();
     }
 
     render() {
-        return (
-            <React.Fragment>
-                <div className="selector">
-                    <div className="J_selectorLine s-line">
+        const attrListDom = this.state.attrList.map((item, index) => {
+            return (
+                <div className="J_selectorLine s-line" key={index}>
                         <div className="sl-wrap">
-                            <div className="sl-key"><strong>适用面积:</strong></div>
+                            <div className="sl-key"><strong>{item.attrName}</strong></div>
                             <div className="sl-value">
                                 <div className="sl-v-list">
                                     <ul className="J_valueList">
-                                        <li>
-                                            <a href="/list.html?cat=1620%2C1624%2C1661&amp;ev=exbrand_%E7%BB%BF%E4%B9%8B%E6%BA%90%5E3707_33710%5E&amp;cid3=1661" rel="nofollow" ><i></i>其它</a>
+                                {
+                                    item.values.map((item1, index1) => {
+                                        return (
+                                        <li key={index1}>
+                                            <a href="/list.html?cat=1620%2C1624%2C1661&amp;ev=exbrand_%E7%BB%BF%E4%B9%8B%E6%BA%90%5E3707_33710%5E&amp;cid3=1661" rel="nofollow" ><i></i>{item1.attrValue}</a>
                                         </li>
-                            
+                                )})}
                                     </ul>
                                 </div>
                                 <div className="sl-btns">
@@ -36,6 +55,13 @@ class App extends React.Component {
                             </div>
                         </div>
                     </div>
+            )
+        })
+
+        return (
+            <React.Fragment>
+                <div className="selector">
+                    {attrListDom}
                 </div>
             </React.Fragment>
         );
