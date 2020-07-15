c679dc4fe0b602bc17d16c9d9216f332bef4e60a
lei_zhou <lei_zhou@epcnszxw0352.princeton.epam.com>
2020-07-15 14:53:51 +0800

    import swagger

--- demo/src/main/java/com/leizhou/controller/HomeController.java
+++ demo/src/main/java/com/leizhou/controller/HomeController.java
@@ -1,12 +1,17 @@
 package com.leizhou.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	org.slf4j.Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
 	@RequestMapping("/")
 	public String home() {
+		logger.debug("1111");
+		logger.error("2222");
 		return "templates/index";
 	}
 }
--- demo/src/main/java/com/leizhou/springboot/webapp/WebappApplication.java
+++ demo/src/main/java/com/leizhou/springboot/webapp/WebappApplication.java
@@ -5,19 +5,27 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.leizhou.mapper.UsersMapper;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @ServletComponentScan("com.leizhou.filter")
 @MappedTypes(UsersMapper.class)
 @MapperScan("com.leizhou.mapper")
-@ComponentScan(basePackages = { "com.leizhou.controller", "com.leizhou.security"} )
+@ComponentScan(basePackages = { "com.leizhou.controller", "com.leizhou.security", "com.leizhou.config"} )
 @EnableTransactionManagement
 public class WebappApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
