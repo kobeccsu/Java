8a044e96fa5dbb4808c86f05a15408c6449f5e94
zhoulei <mail.lei.zhou@gmail.com>
2021-01-29 01:32:01 +0800

    add swagger

--- demo/src/main/java/com/leizhou/config/SpringFoxConfig.java
+++ demo/src/main/java/com/leizhou/config/SpringFoxConfig.java
@@ -2,6 +2,7 @@ package com.leizhou.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,7 +13,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
-
+@Component
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig implements WebMvcConfigurer {
