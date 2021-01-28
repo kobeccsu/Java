476bb4aa3496466c14001a1f20b5241d5f0e2c56
zhoulei <mail.lei.zhou@gmail.com>
2021-01-29 01:16:10 +0800

    add swagger

--- demo/src/main/java/com/leizhou/springboot/webapp/WebappApplication.java
+++ demo/src/main/java/com/leizhou/springboot/webapp/WebappApplication.java
@@ -8,14 +8,15 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.leizhou.mapper.UsersMapper;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+@EnableSwagger2
 @SpringBootApplication
 @ServletComponentScan("com.leizhou.filter")
 @MappedTypes(UsersMapper.class)
 @MapperScan("com.leizhou.mapper")
 @ComponentScan("com.leizhou.security")
-@ComponentScan(basePackages = { "com.leizhou.controller", "com.leizhou.security"} )
+@ComponentScan(basePackages = { "com.leizhou.controller", "com.leizhou.security","com.leizhou.config"} )
 public class WebappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
